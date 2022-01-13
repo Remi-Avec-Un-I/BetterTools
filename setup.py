a151e1b0ed701198f0af0f4f5aec72d02f1b43e3
Remi-Avec-Un-I <74991151+Remi-Avec-Un-I@users.noreply.github.com>
2022-01-13 14:41:47 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+classifiers=[
+    'Development Status :: 1 - Alpha',
+    'Intended Audience :: Developers',
+    'Operating System :: MacOS :: Windows 10/11'
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+]
+
+setup(
+    name='BetterTools',
+    version='0.0.1',
+    description='This is a lib to improove input fonction.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Remi_Avec_Un_I',
+    
+)

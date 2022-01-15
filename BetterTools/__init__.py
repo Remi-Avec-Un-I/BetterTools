494ebe7b891b76067d66b6391532cd345b8352e1
Remi-Avec-Un-I <74991151+Remi-Avec-Un-I@users.noreply.github.com>
2022-01-15 02:06:59 +0100

    Update __init__.py

--- BetterTools/__init__.py
+++ BetterTools/__init__.py
@@ -1,6 +1,14 @@
 import os
 import time
     
+"""
+    TODO:
+        ->  Bprint() with speed option
+        ->  function to setup arguments that will always be the same for a function
+        ->  Update docs
+"""
+
+
 def cls():
     if os.name in ('nt', 'dos'):
         os.system("cls")
@@ -14,11 +22,6 @@ def Binput(Input_message = "", Input_type = "str", error_message = "", clear = F
     I recommend to write all arguments if you want to callback a func,
     for the function arguments you have to assign var to values, like :
     Binput(Input_message=..., ..., delay=..., func=function to callback, func_args="hahahahahaha", other_func_args="test")
-    """
-    
-    """
-    TODO:
-        ->
     """
     try:
         return Input_type(input(Input_message))
@@ -35,7 +38,20 @@ def Binput(Input_message = "", Input_type = "str", error_message = "", clear = F
             print(f"You didn't write an {Input_type.__name__} !")
         if func:
             func(**kwargs)
-                
-                
-Binput(Input_message="test :", Input_type="int", error_message="You had to write a number", clear=True, delay=1)
-print("yax")
+            
+def Btype(var, print=False):
+    """
+    Like type()function but return the name only name of a var like: int, not <class 'int'>
+    But can print automatically, or return
+    """
+    if print:
+        print(type(var).__name__)
+    else:
+        return type(var).__name__
+
+# some function to make coding faster, but not more easier to understand
+def i(text):    
+    return input(text)
+
+def p(text):
+    return print(text)

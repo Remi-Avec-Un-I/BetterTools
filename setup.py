57bbf0c95a8c89c394872c823d3db5d9e5329079
Remi-Avec-Un-I <74991151+Remi-Avec-Un-I@users.noreply.github.com>
2022-01-15 02:07:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, find_packages
 classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
-    'Operating System :: Microsoft :: Windows :: Windows 10'
+    'Operating System :: Microsoft :: Windows :: Windows 10',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3'
 ]
 
 setup(
     name='BetterTools',
-    version='0.0.1',
+    version='0.0.5',
     description='This is a lib to improove input fonction.',
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url='',
+    url='https://github.com/Remi-Avec-Un-I/BetterTools',
     author='Remi_Avec_Un_I',
-    
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['input', 'BetterTools', 'Tools', 'Better'],
+    packages=find_packages(),
+    install_requires=[""]
 )
